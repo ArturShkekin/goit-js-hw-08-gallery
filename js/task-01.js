cf4736d0bafebc8842d3ea98af97a706ff76105e
Artur Shkekin <64356091+ArturShkekin@users.noreply.github.com>
2021-04-29 21:39:25 +0300

    что то

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,17 @@ import images from "./gallery-items.js"
 const setGallery = document.querySelector(".gallery");
 
 images.forEach(image => {
-  const listString = `<li class="gallery__item"><a class="gallery__link" href=${image.original}>
-  <img class="gallery__image" src=${image.preview} data-source=${image.original} alt=${image.description}></a></li>`;
-  setGallery.innerHTML += listString;
+  setGallery.insertAdjacentHTML("beforeend", `<li class="gallery__item"><a class="gallery__link" href=${image.original}>
+  <img class="gallery__image" src=${image.preview} data-source=${image.original} alt=${image.description}></a></li>`)
+  
 })
+
+const openModalWindow = document.querySelector(".lightbox");
+
+const closeModalWindow = document.querySelector(".lightbox__button");
+
+const backdrop = document.querySelector(".lightbox__overlay");
+const toggleBackdrop = () => alert();
+
+setGallery.firstElementChild.addEventListener("click", toggleBackdrop);
+//closeModalWindow.addEventListener("click", toggleBackdrop);
