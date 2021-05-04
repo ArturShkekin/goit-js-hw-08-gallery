f508b67f6341d13119f892d2a028a4b8d6d308c1
Artur Shkekin <64356091+ArturShkekin@users.noreply.github.com>
2021-05-04 21:33:29 +0300

    Сделал открытие и закрытие модалки

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,4 @@
-import images from "./gallery-items.js"
+import images from "./gallery-items.js";
 
 const setGallery = document.querySelector(".gallery");
 
@@ -8,12 +8,37 @@ images.forEach(image => {
   
 })
 
-const openModalWindow = document.querySelector(".lightbox");
-
-const closeModalWindow = document.querySelector(".lightbox__button");
-
+const onOpenModal = document.querySelector(".lightbox");
+const onCloseModal = document.querySelector(".lightbox__button");
+const modalImg = document.querySelector(".lightbox__image");
 const backdrop = document.querySelector(".lightbox__overlay");
-const toggleBackdrop = () => alert();
+const linkEl = document.querySelector(".gallery__link");
+const imgGallery = document.querySelector(".gallery__image");
+const itemGallery = document.querySelector(".gallery__item");
 
-setGallery.firstElementChild.addEventListener("click", toggleBackdrop);
-//closeModalWindow.addEventListener("click", toggleBackdrop);
+/*const  onEmage = () => {
+  
+ console.log("ОППА");
+  
+  //onOpenModal.classList.toggle("is-open");
+  //onCloseModal.classList.toggle("is-open");
+  
+  //modalImg.src = event.target.dataset.source;
+  
+};*/
+
+const onEmage = (event) => {
+  event.preventDefault();
+  onOpenModal.classList.toggle("is-open");
+  modalImg.src = event.currentTarget.dataset.source;
+  linkEl.href = "";
+};
+
+imgGallery.addEventListener("click", onEmage);
+
+const onClose = () => {
+onOpenModal.classList.toggle("is-open");
+};
+
+onCloseModal.addEventListener("click", onClose);
+
